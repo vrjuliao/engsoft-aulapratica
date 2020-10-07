43f437cb9e130aa9cd4c390fc0d44921cae3aae9
V. Julião <viniciusjuliao@dcc.ufmg.br>
2020-10-07 01:43:26 -0300

    Third commit

--- src/main/java/main/Money.java
+++ src/main/java/main/Money.java
@@ -1,10 +1,20 @@
 package main;
 
-public class Money {
+abstract public class Money {
     protected int amount;
 
-    public boolean equals(Object object)  {
+    public boolean equals(Object object) {
         Money money = (Money) object;
-        return amount == money.amount;
+        return amount == money.amount && getClass().equals(money.getClass());
     }
+
+    public static Money dollar(int amount)  {
+        return new Dollar(amount);
+    }
+
+    public static Money franc(int amount) {
+        return new Franc(amount);
+    }
+
+    abstract public Money times(int multiplier);
 }
