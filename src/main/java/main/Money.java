f9e31dcb4858d95bf6df86c767af6a34dfbe06d6
V. Julião <viniciusjuliao@dcc.ufmg.br>
2020-10-07 02:16:06 -0300

    Last commit

--- src/main/java/main/Money.java
+++ src/main/java/main/Money.java
@@ -1,6 +1,6 @@
 package main;
 
-abstract public class Money {
+public class Money {
     protected int amount;
     protected String currency;
 
@@ -11,21 +11,23 @@ abstract public class Money {
 
     public boolean equals(Object object) {
         Money money = (Money) object;
-        return amount == money.amount && getClass().equals(money.getClass());
+        return amount == money.amount && currency().equals(money.currency());
     }
 
     public static Money dollar(int amount)  {
-        return new Dollar(amount, "USD");
+        return new Money(amount, "USD");
     }
 
     public static Money franc(int amount) {
-        return new Franc(amount, "CHF");
+        return new Money(amount, "CHF");
     }
 
     public String currency() {
         return currency;
     }
 
-    abstract public Money times(int multiplier);
+    public Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    }
 
 }
